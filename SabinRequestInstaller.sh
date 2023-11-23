e69c8c4f1d7aa205e0fb55904b3b265f0c22f93b
Nebula-Manta <127610864+Nebula-Manta@users.noreply.github.com>
2023-11-23 18:47:54 +0000

    Create SabinRequestInstaller.sh

---
+++ SabinRequestInstaller.sh
@@ -0,0 +1,4 @@
+cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
+echo -e "Downloading..."
+curl "https://raw.githubusercontent.com/Nebula-Manta/Themes/main/SabinRequest.js" -o "./theme-tomorrow_night.js"
+echo -e "Theme installed by Manta, btw you still a FAILURE"
